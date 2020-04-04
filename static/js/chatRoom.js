678e363ec09e1b270f94f9c09bdf3eb729844182
Jerry <gaozhiwei1993@gmail.com>
2020-04-04 20:10:08 +0900

    url fixed

--- static/js/chatRoom.js
+++ static/js/chatRoom.js
@@ -60,9 +60,9 @@ $(function() {
         var $messageDiv;
         // var $rand = $('#rand').val()
         if (type == 0) {
-            var $useravatar = $('<img height="80px" style="margin-right: 10px;margin-bottom: 40px" src="/static/img/avatar/avatar%20(' + r + ').png"/>');
+            var $useravatar = $('<img height="60px" style="margin-right: 10px;margin-bottom: 40px" src="/static/img/avatar/avatar%20(' + r + ').png"/>');
             if (name === 'gao' || name === 'Gao') {
-                $useravatar = $('<img height="80px" style="margin-right: 10px;margin-bottom: 40px" src="/static/img/icon.png"/>');
+                $useravatar = $('<img height="60px" style="margin-right: 10px;margin-bottom: 40px" src="/static/img/icon.png"/>');
             }
             var $usernameDiv = $('<span style="margin-right: 15px;font-weight: 700;overflow: hidden;text-align: right;"/>')
                     .text(name);
