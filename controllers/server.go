a1be1f7d11113efa1f75ed3474d3159708314e1a
Jerry <gaozhiwei1993@gmail.com>
2020-04-22 15:23:51 +0900

    fixed

--- controllers/server.go
+++ controllers/server.go
@@ -79,7 +79,7 @@ func (c *ServerController) WS() {
 			break
 		}
 
-		beego.Info("WS-----------receive: " + string(msgStr))
+		beego.Info("WSS-----------receive: " + string(msgStr))
 
 		//如果没有错误，则把用户发送的信息放入message通道中
 		var msg Message
