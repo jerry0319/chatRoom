5f984394d90416a5f4e2709279385d762d5fd438
Jerry <gaozhiwei1993@gmail.com>
2020-04-22 14:03:53 +0900

    fixed

--- routers/router.go
+++ routers/router.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init() {
-	beego.Router("/cr", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 	beego.Router("/chatRoom", &controllers.ServerController{})
 	beego.Router("/chatRoom/WS", &controllers.ServerController{}, "get:WS")
 }
