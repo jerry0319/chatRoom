342a749c8310582afa129392fff71b5b38b03eac
Jerry <gaozhiwei1993@gmail.com>
2020-04-28 00:25:09 +0900

    fixed

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"strings"
 )
 
 type MainController struct {
@@ -9,5 +11,10 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	c.Data["prodPath"] = ""
+	if strings.EqualFold("prod", beego.BConfig.RunMode) {
+		fmt.Println("in prod mode, using prod path.")
+		c.Data["prodPath"] = "/chatRoom/"
+	}
 	c.TplName = "index.html"
 }
