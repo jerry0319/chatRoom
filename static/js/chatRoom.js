b17dcb14142adf542472f2907c5cb5d0fec3a36c
Jerry <gaozhiwei1993@gmail.com>
2020-04-27 20:17:24 +0900

    fixed

--- static/js/chatRoom.js
+++ static/js/chatRoom.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     //=========================初始化====================================
     var $window = $(window);
     var $messageArea = $('#messageArea');       // 消息显示的区域
@@ -7,7 +7,7 @@ $(function() {
     var connected = false;                      // 用来判断是否连接的标志
 
     // 通过一个hash函数得到用户名的颜色
-    function getUsernameColor (username) {
+    function getUsernameColor(username) {
         var COLORS = [
             '#e21400', '#91580f', '#f8a700', '#f78b00',
             '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
@@ -24,7 +24,7 @@ $(function() {
     }
 
     // 初始化显示的名字颜色
-    var nameColor = getUsernameColor( $("#name").text());
+    var nameColor = getUsernameColor($("#name").text());
     $("#name").css('color', nameColor);
 
     Date.prototype.format = function (fmt) { // author: meizz
@@ -46,7 +46,7 @@ $(function() {
 
     //====================webSocket连接======================
     // 创建一个webSocket连接
-    var socket = new WebSocket('ws://'+window.location.host+'/cr/chatRoom/WS?name=' + $('#name').text() + '&rand=' + $('#rand').val());
+    var socket = new WebSocket('ws://' + window.location.host + '/chatRoom/WS?name=' + $('#name').text() + '&rand=' + $('#rand').val());
 
     // 当webSocket连接成功的回调函数
     socket.onopen = function () {
@@ -61,27 +61,16 @@ $(function() {
     };
 
     //=======================接收消息并显示===========================
-    // 接受webSocket连接中，来自服务端的消息
-    socket.onmessage = function(event) {
-        // 将服务端发送来的消息进行json解析
-        var data = JSON.parse(event.data);
-        console.log("revice:" , data);
+    var $messageDiv;
 
-        var name = data.name;
-        var type = data.type;
-        var msg = data.message;
-        var r = data.random
-
-        // type为0表示有人发消息
-        var $messageDiv;
-        // var $rand = $('#rand').val()
-        if (type == 0) {
-            var $useravatar = $('<img height="70px" style="margin-right: 10px;margin-bottom: 37px" src="/cr/static/img/avatar/avatar%20(' + r + ').png"/>');
+    function extend_message(r, name, msg, type) {
+        if (type === 0) {
+            var $useravatar = $('<img height="70px" style="margin-right: 10px;margin-bottom: 37px" src="../static/img/avatar/avatar%20(' + r + ').png"/>');
             if (name === 'gao' || name === 'Gao') {
-                $useravatar = $('<img height="70px" style="margin-right: 10px;margin-bottom: 37px" src="/cr/static/img/icon.png"/>');
+                $useravatar = $('<img height="70px" style="margin-right: 10px;margin-bottom: 37px" src="../static/img/icon.png"/>');
             }
             var $usernameDiv = $('<span style="margin-right: 15px;font-weight: 700;overflow: hidden;text-align: right;"/>')
-                    .text(name);
+                .text(name);
             if (name == $("#name").text()) {
                 $usernameDiv.css('color', nameColor);
             } else {
@@ -100,20 +89,37 @@ $(function() {
             $usernamemsg.append($username, $messageBodyDiv)
 
             $messageDiv = $('<li style="list-style-type:none;font-size:20px;margin-bottom: -30px"/>')
-                    .data('username', name)
-                    // .append($usernameDiv, $messageBodyDiv);
-                    .append($useravatar, $usernamemsg);
+                .data('username', name)
+                // .append($usernameDiv, $messageBodyDiv);
+                .append($useravatar, $usernamemsg);
         }
         // type为1或2表示有人加入或退出
         else {
             var $messageBodyDiv = $('<span style="color:#999999;">')
-                    .text(msg + "　" + new Date().format("yyyy-MM-dd hh:mm:ss"));
+                .text(msg + "　" + new Date().format("yyyy-MM-dd hh:mm:ss"));
             $messageDiv = $('<li style="list-style-type:none;font-size:15px;text-align:center;"/>')
-                    .append($messageBodyDiv);
+                .append($messageBodyDiv);
         }
 
         $messageArea.append($messageDiv);
         $messageArea[0].scrollTop = $messageArea[0].scrollHeight;   // 让屏幕滚动
+
+    }
+
+// 接受webSocket连接中，来自服务端的消息
+    socket.onmessage = function (event) {
+        // 将服务端发送来的消息进行json解析
+        var data = JSON.parse(event.data);
+        console.log("revice:", data);
+
+        var name = data.name;
+        var type = data.type;
+        var msg = data.message;
+        var r = data.random
+
+        // type为0表示有人发消息
+        // var $rand = $('#rand').val()
+        extend_message(r, name, msg, type)
     }
 
     //========================发送消息==========================
@@ -121,17 +127,25 @@ $(function() {
     $window.keydown(function (event) {
         // 13是回车的键位
         if (event.which === 13) {
-            sendMessage();
+            sendMessage(1);
             typing = false;
         }
     });
 
     // 发送按钮点击事件
-    $("#sendBtn").click(function () {
-        sendMessage();
+    $("#sendBtn1").click(function () {
+        sendMessage(1);
     });
 
-    $(document).on('click', '#re_send', function() {
+    $("#sendBtn2").click(function () {
+        sendMessage(2);
+    });
+
+    $("#sendBtn3").click(function () {
+        sendMessage(3);
+    });
+
+    $(document).on('click', '#re_send', function () {
         var inputMessage = $inputArea.val();
         if (inputMessage) {
             sendToSocket(inputMessage);
@@ -141,11 +155,14 @@ $(function() {
     });
 
     // 通过webSocket发送消息到服务端
-    function sendMessage () {
+    function sendMessage(type) {
         var inputMessage = $inputArea.val();  // 获取输入框的值
 
-        const predictUrl = "http://aoi.naist.jp/predict";
-        var $data = JSON.stringify({"text":inputMessage});
+        // const predictUrl = "http://aoi.naist.jp/predict";
+        const predictUrl = "http://127.0.0.1:8091/";
+        var $data = JSON.stringify({"text": inputMessage, "type": type});
+        var r = $('#rand').val()
+        var nam = $('#name').text()
 
         if (inputMessage) {
             $.ajax({
@@ -155,22 +172,33 @@ $(function() {
                 data: $data,
                 dataType: "json",
                 async: false,
-                success:function (message) {
-                    if (message.result === "Offensive") {
-                        // alert("Offensive");
-                        $('#myModal').modal('show');
-                        $('#myModal').on('hidden.bs.modal', function (e) {
-                            $inputArea.focus();
-                        })
+                success: function (message) {
+                    if (message.result !== -1) { //offensive
+                        if (type === 1){
+                            // show_modal();
+                            extend_message(r, nam, inputMessage, 0);
+                        } else if (type === 2) {
+                            // show_modal();
+                            sendToSocket(message.result);
+                        } else if (type === 3) {
+                            sendToSocket(message.result)
+                        }
                     } else {
                         sendToSocket(inputMessage);
                     }
                 },
-                error:function (message) {
+                error: function (message) {
                     alert("fail: " + message);
                 }
             });
         }
+    }
+
+    function show_modal() {
+        $('#myModal').modal('show');
+        $('#myModal').on('hidden.bs.modal', function (e) {
+            $inputArea.focus();
+        });
     }
 
     function sendToSocket(inputMessage) {
